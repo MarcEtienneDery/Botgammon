e82a8e5a2424a489590b677138734698c320c14a
marcetiennedery <derymarcetienne@gmail.com>
2014-11-19 15:50:41 -0500

    première ébauche: ajout de classe player, move et position. interface pour interagir avec gnubg

--- BotGammon/BotGammon/Program.cs
+++ BotGammon/BotGammon/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Botgammon
@@ -10,7 +14,59 @@ namespace Botgammon
     {
         static void Main(string[] args)
         {
-            // Frank test    
+
+            String gnubg = "D:\\Games\\gnubg\\gnubg-cli.exe"; //TODO get from arguments
+
+            //Start gnubg-cli
+            ProcessStartInfo startInfo = new ProcessStartInfo(gnubg);
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.RedirectStandardInput = true;
+            startInfo.UseShellExecute = false;
+
+            Process process = Process.Start(startInfo);
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
+            process.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);
+
+            // start a new game
+            process.StandardInput.WriteLine("new game");
+
+            int coupCounter = 0;
+            IPlayer player = new Player();
+            bool gameFinished = false;
+
+            while (!gameFinished)// boucle pour chaque coup qu'on doit jouer.
+            {
+                // on se prépare à jouer le prochain coup.
+                process.StandardInput.WriteLine("roll"); // on roll les dés.
+                string exportFile = Directory.GetCurrentDirectory() + "\\export" + coupCounter + ".txt";
+                coupCounter ++;
+                process.StandardInput.WriteLine("export position snowie " + exportFile);
+
+                // on load le fichier snowie
+                while (!File.Exists(exportFile)) // truc guetto pour attendre l'export du fichier.
+                {
+                    Thread.Sleep(10);
+                }
+
+                StreamReader sr = new StreamReader(exportFile);
+                   
+                String line = sr.ReadToEnd();
+                // TODO parsing du fichier snowie.
+                Position pos = new Position(line);
+                sr.Close();
+                File.Delete(exportFile);// remove the old file after being done with it.
+
+                Move nextMove = player.GetNextMove(pos);// we ask for the next move to make.
+
+                process.StandardInput.WriteLine(nextMove.getCmd());
+
+                // TODO trouver comment une game fini. et changer le bool.
+                gameFinished = true;
+            }
+            process.StandardInput.WriteLine("save game " + Directory.GetCurrentDirectory() + "\\tester.sgf");
         }
     }
 }
---
+++ Botgammon/Botgammon/Move.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botgammon
+{
+    class Move
+    {
+        public Move(int a, int newA)
+        {
+            moveA = new Tuple<int, int>(a, newA);
+        }
+
+        public Move(int a, int newA, int b, int newB)
+        {
+            moveA = new Tuple<int, int>(a, newA);
+            moveB = new Tuple<int, int>(b, newB);
+        }
+
+        public Move(int a, int newA, int b, int newB, int c, int newC)
+        {
+            moveA = new Tuple<int, int>(a, newA);
+            moveB = new Tuple<int, int>(b, newB);
+            moveC = new Tuple<int, int>(c, newC);
+        }
+
+        public Move(int a, int newA, int b, int newB, int c, int newC, int d, int newD)
+        {
+            moveA = new Tuple<int, int>(a, newA);
+            moveB = new Tuple<int, int>(b, newB);
+            moveC = new Tuple<int, int>(c, newC);
+            moveD = new Tuple<int, int>(d, newD);
+        }
+
+        public string getCmd()
+        {
+            string cmd = "move ";
+            if (moveA != null)
+            {
+                cmd += moveA.Item1 + "/" + moveA.Item2 + " ";
+            }
+            if (moveB != null)
+            {
+                cmd += moveB.Item1 + "/" + moveB.Item2 + " ";
+            }
+            if (moveC != null)
+            {
+                cmd += moveC.Item1 + "/" + moveC.Item2 + " ";
+            }
+            if (moveD != null)
+            {
+                cmd += moveD.Item1 + "/" + moveD.Item2 + " ";
+            }
+            return cmd;
+        }
+
+        private Tuple<int, int> moveA;
+        private Tuple<int, int> moveB;
+        private Tuple<int, int> moveC;
+        private Tuple<int, int> moveD;
+    }
+}
