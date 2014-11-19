62064730ec5f5c97c12d5df8c69fdd3154f47a2b
marcetiennedery <derymarcetienne@gmail.com>
2014-11-19 16:17:39 -0500

    trying fix

--- BotGammon/BotGammon/Program.cs
+++ BotGammon/BotGammon/Program.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Botgammon
+namespace BotGammon
 {
     class Program
     {
--- Botgammon/Botgammon/Position.cs
+++ Botgammon/Botgammon/Position.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Botgammon
+namespace BotGammon
 {
     class Position
     {
