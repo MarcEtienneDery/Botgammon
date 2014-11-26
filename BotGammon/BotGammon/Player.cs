422a4af26d6306a46a1b637da1e9eb92d4214461
DomLatr <dom.latr@gmail.com>
2014-11-26 17:09:08 -0500

    Into the pit of the ExpectMinimax(gossage)

--- BotGammon/BotGammon/Player.cs
+++ BotGammon/BotGammon/Player.cs
@@ -10,7 +10,53 @@ namespace BotGammon
     {
         public Move GetNextMove(Position pos)
         {
+
+
             return new Move(1,2);
         }
+
+        public Move ExpectMinimax(Position pos, int profondeur, int action)
+        {
+
+            if (profondeur == 0)
+            {
+                //Need ajout fonction heuristique
+                return null;
+            }
+
+            if (action == 1)
+            {
+
+                
+            }
+            else if (action == 2)
+            {
+                
+            }
+            else if (action == 3)
+            {
+                for (int i = 1; i < 7; i++)
+                {
+                    for (int j = i; j < 7; j++)
+                    {
+
+                    }
+                }
+            }
+            return null;
+
+        }
+
+        public int[] ProbabilityDice()
+        {
+            var listeDice = new List<Tuple<int, int>>();
+
+
+
+            return null;
+        }
+
+
+
     }
 }
